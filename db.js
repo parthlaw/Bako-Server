73bfb39f8af70be4637d44aa637d897d43e4f4db
Parth <img_2019045@iiitm.ac.in>
2020-08-29 13:15:44 +0530

    changed db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Pool, Client } = require('pg');
 const pool = new Pool({
-	user: 'postgres',
-	host: '127.0.0.1',
-	database: 'bako',
-	password: '1234',
+	user: 'vodacnmqwkqvtt',
+	host: 'ec2-34-198-243-120.compute-1.amazonaws.com',
+	database: 'd9kcrqk0fibjl0',
+	password: '69ab9624cac81744805c4d3b4c6b163a16322f063030defc44c06b0fe99a8466',
 	port: 5432
 });
 module.exports = {
